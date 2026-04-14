8107b89f39d30e0ca45a12da8de8007c6dcad5a0
Brainito1101 <accounts@brainito.com>
2026-04-14 23:51:37 +0530

    robots file update

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,12 +1,41 @@
 import { MetadataRoute } from 'next';
 
+import { SITE_URL } from '@/lib/page-metadata-defaults';
+
+const DISALLOW = ['/auth/', '/logout-sync', '/api/', '/_next/'] as const;
+
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: '*',
-      allow: '/',
-      disallow: ['/auth/', '/logout-sync', '/api/', '/_next/'],
-    },
-    sitemap: 'https://brainito.com/sitemap.xml',
+    rules: [
+      {
+        userAgent: '*',
+        allow: '/',
+        disallow: [...DISALLOW],
+        crawlDelay: 1,
+      },
+      {
+        userAgent: 'Googlebot',
+        allow: '/',
+        disallow: [...DISALLOW],
+        crawlDelay: 0,
+      },
+      {
+        userAgent: 'Bingbot',
+        allow: '/',
+        disallow: [...DISALLOW],
+        crawlDelay: 1,
+      },
+      {
+        userAgent: 'Twitterbot',
+        allow: '/',
+        disallow: [...DISALLOW],
+      },
+      {
+        userAgent: 'facebookexternalhit',
+        allow: '/',
+        disallow: [...DISALLOW],
+      },
+    ],
+    sitemap: `${SITE_URL}/sitemap.xml`,
   };
 }
